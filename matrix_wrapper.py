fixed matrix element calculation + matrix element integration

matrix_wrapper.py
@@ -11,7 +11,18 @@ import matrix2py
 
 
 def initialisemodel():
+    print("Initializing model")
     matrix2py.initialisemodel(root_path+"/mg5/fortran_output/Cards/param_card.dat")
 
 def smatrix(tensor: np.ndarray):
-    return matrix2py.smatrix(tensor.tolist())
+    print("Calculating matrix element")
+
+    result = np.array(
+        [
+            matrix2py.smatrix(row.T.tolist())
+            for row in tensor
+        ],
+        dtype=np.float32
+    )
+
+    return result